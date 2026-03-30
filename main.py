8b7ffc74f6e80a723ef8a66af6808015d25c5e99
rivasdanielcas12 <daniiedu.14@gmail.com>
2026-03-30 05:47:25 -0500

    primer commit

---
+++ main.py
@@ -0,0 +1,6 @@
+import os
+from dotenv import load_dotenv
+
+if __name__ == "__main__":
+    load_dotenv()  # Cargar variables de entorno desde el archivo .env
+    print("🚀 Proyecto iniciado correctamente")
